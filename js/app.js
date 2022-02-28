02a78154f2bae880fc79c5a93f9240711c442629
Salman <you@example.com>
2022-02-28 23:19:41 +0600

    data loaded

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('jaksh')
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchInput = document.getElementById("search-input");
+  const searchText = searchInput.value;
+  searchInput.value = "";
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => loadPhones(data.data));
+});
+
+const loadPhones = (phones)=>{
+    // console.log(phones)
+    phones.forEach(phone => {
+        console.log(phone)
+    });
+};
