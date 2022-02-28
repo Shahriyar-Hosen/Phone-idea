3edaee5c491ba1f0df79932c0a4972966c8b3ae1
Salman <you@example.com>
2022-02-28 23:51:50 +0600

    display search result

--- js/app.js
+++ js/app.js
@@ -8,9 +8,26 @@ document.getElementById("search-btn").addEventListener("click", () => {
     .then((data) => loadPhones(data.data));
 });
 
-const loadPhones = (phones)=>{
-    // console.log(phones)
-    phones.forEach(phone => {
-        console.log(phone)
-    });
+const loadPhones = (phones) => {
+  const searchResult = document.getElementById("search-result");
+  phones.forEach((phone) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+      <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top  p-3" alt="Phone images" />
+            <div class="card-body">
+              <h4 class="card-title text-center">${phone.phone_name}</h4>
+              <h6 class="card-text text-center">
+                ${phone.brand}
+              </h6>
+            </div>
+            <div class="mx-auto m-3">
+              <button class="btn btn-primary">See Detiles</button>
+            </div>
+          </div>
+      `;
+      searchResult.appendChild(div)
+    console.log(phone);
+  });
 };
