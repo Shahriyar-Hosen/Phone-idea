448e08696e51624fa80acedfe44ba33b5b264a1a
Salman <you@example.com>
2022-03-01 11:24:48 +0600

    phone detiles add

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ document.getElementById("search-btn").addEventListener("click", () => {
 
 const loadPhones = (phones) => {
   const searchResult = document.getElementById("search-result");
+  searchResult.textContent = "";
   phones.forEach((phone) => {
     const div = document.createElement("div");
     div.classList.add("col");
@@ -27,15 +28,51 @@ const loadPhones = (phones) => {
             </div>
           </div>
       `;
-      searchResult.appendChild(div)
-    // console.log(phone.slug);
+    searchResult.appendChild(div);
   });
 };
 
-const loadDetiles = (phoneId)=>{
+const loadDetiles = (phoneId) => {
   const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
   fetch(url)
-  .then(res=>res.json())
-  .then(data=>console.log(data))
+    .then((res) => res.json())
+    .then((data) => displayDetiles(data.data));
 };
 
+const displayDetiles = (detiles) => {
+  console.log(detiles);
+  const detilesConteinar = document.getElementById("phone-detiles");
+  detilesConteinar.textContent = "";
+  const others = (data) => {
+    if (data == undefined || data == null) {
+      console.log("No other specialties");
+      return "No other specialties"
+    }
+  };
+  const othersData = others(detiles.others);
+  const div = document.createElement("div");
+  div.classList.add("row");
+  div.innerHTML = `
+           <div class="col-md-4">
+              <img src="${
+                detiles.image
+              }" class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${
+                  detiles.name
+                } - <small class="fs-6 fw-light">(${detiles.brand})</small></h5>
+                <p>${detiles.releaseDate}</p>
+                <p>Main Features:
+                <br>Chip Set: ${detiles.mainFeatures.chipSet} 
+                <br>Display Size: ${detiles.mainFeatures.displaySize}
+                <br>Memory: ${detiles.mainFeatures.memory}</p>
+                <p>Others:
+                <br></p>
+                <p></p>
+              </div>
+           </div>
+  `;
+  detilesConteinar.appendChild(div);
+};
